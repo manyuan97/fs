aff8e5dabdea15fa3c9ad31e90a37dfe09a63fd7
张满园 <zhangmanyuan@zhangmanyuandeMacBook-Pro.local>
2024-03-18 17:08:30 +0800

    first commit

--- run_model_cross_validation.py
+++ run_model_cross_validation.py
@@ -1,15 +1,14 @@
-import gc
+import argparse
 import os
 
 import numpy as np
-import argparse
 
 from core.data_helper import DataProcessor
 from core.eval_helper import ModelEvaluator
 from core.model_helper import FeatureSelectorByModel
 from core.model_helper import ModelTrainer
 from core.result_helper import ResultSaver
-from core.util_helper import visualize_selected_features,load_params_from_file
+from core.util_helper import visualize_selected_features, load_params_from_file
 
 
 def main(target_column, model_name, model_params_path, regressor_name, regressor_params_path, save_dir):
@@ -24,6 +23,7 @@ def main(target_column, model_name, model_params_path, regressor_name, regressor
     data_processor = DataProcessor(target_column=target_column)
     X_train, y_train, scaler = data_processor.load_and_preprocess_data(train_file_path)
     X_val, y_val, _ = data_processor.load_and_preprocess_data(val_file_path, scaler=scaler)
+    X_test, y_test, _ = data_processor.load_and_preprocess_data(test_file_path, scaler=scaler)
 
     model_trainer = ModelTrainer(model_name=model_name, params=model_params)
     xgboost_model = model_trainer.train_model(X_train, y_train)
@@ -59,11 +59,17 @@ def main(target_column, model_name, model_params_path, regressor_name, regressor
         regressor = regressor_trainer.train_model(select_X_train, y_train)
 
         select_X_val = selector.transform(X_val)
-
+        select_X_test = selector.transform(X_test)
 
         model_evaluator = ModelEvaluator(regressor)
         metrics_train = model_evaluator.evaluate_model(select_X_train, y_train)
         metrics_val = model_evaluator.evaluate_model(select_X_val, y_val)
+        metrics_test = model_evaluator.evaluate_model(select_X_test, y_test)
+
+        metrics_train_pos = model_evaluator.evaluate_model(select_X_train, y_train, filter_positive_pred=True)
+        metrics_val_pos = model_evaluator.evaluate_model(select_X_val, y_val, filter_positive_pred=True)
+        metrics_test_pos = model_evaluator.evaluate_model(select_X_test, y_test, filter_positive_pred=True)
+
         mse = metrics_val.__dict__['pearson_corr_without_mean']
         print(f"Thresh={thresh:.3f}, n={select_X_train.shape[1]}, MSE: {mse:.2f}")
 
@@ -75,6 +81,10 @@ def main(target_column, model_name, model_params_path, regressor_name, regressor
             'pearson_corr_without_mean': mse,
             'train': metrics_train.__dict__,
             'val': metrics_val.__dict__,
+            'test': metrics_test.__dict__,
+            'train_pos': metrics_train_pos.__dict__,
+            'val_pos': metrics_val_pos.__dict__,
+            'test_pos': metrics_test_pos.__dict__,
             'selected_features': selected_features.tolist(),
         })
 
@@ -92,27 +102,17 @@ def main(target_column, model_name, model_params_path, regressor_name, regressor
     model_evaluator = ModelEvaluator(best_model)
 
     metrics_train = model_evaluator.evaluate_model(best_X_selected, y_train)
-    metrics_train_positive = model_evaluator.evaluate_model(best_X_selected, y_train,filter_positive_pred=True)
-
-
-    del X_train, y_train, X_train_selected, best_X_selected
-    gc.collect()
+    metrics_train_positive = model_evaluator.evaluate_model(best_X_selected, y_train, filter_positive_pred=True)
 
     X_val_selected = best_selector.transform(X_val)
 
     metrics_val = model_evaluator.evaluate_model(X_val_selected, y_val)
-    metrics_val_positive = model_evaluator.evaluate_model(X_val_selected, y_val,filter_positive_pred=True)
+    metrics_val_positive = model_evaluator.evaluate_model(X_val_selected, y_val, filter_positive_pred=True)
 
-
-    del X_val, y_val, X_val_selected
-    gc.collect()
-
-    X_test, y_test, _ = data_processor.load_and_preprocess_data(test_file_path, scaler=scaler)
     X_test_selected = best_selector.transform(X_test)
 
     metrics_test = model_evaluator.evaluate_model(X_test_selected, y_test)
-    metrics_test_positive = model_evaluator.evaluate_model(X_test_selected, y_test,filter_positive_pred=True)
-
+    metrics_test_positive = model_evaluator.evaluate_model(X_test_selected, y_test, filter_positive_pred=True)
 
     final_metrics = {
         'model_name': model_name,
@@ -127,7 +127,7 @@ def main(target_column, model_name, model_params_path, regressor_name, regressor
             'test_pos': metrics_test_positive.__dict__,
 
         },
-        'args':args_dict,
+        'args': args_dict,
         'selected_features': best_selected_feature.tolist(),
 
     }
@@ -140,22 +140,19 @@ def main(target_column, model_name, model_params_path, regressor_name, regressor
     print(f"All metrics stored in {json_file_path}")
 
 
-
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run model training and evaluation pipeline.")
     parser.add_argument("--target_column", type=str, default='y1', help="Name of the target column.")
     parser.add_argument("--model_name", type=str, default="xgboost", help="Name of the initial model.")
     parser.add_argument("--save_dir", type=str, default='results/xgboost_cross', help="Directory to save the results.")
-    parser.add_argument('--regressor_name', type=str, default='linear_regression', help='The target column for prediction.')
+    parser.add_argument('--regressor_name', type=str, default='linear_regression',
+                        help='The target column for prediction.')
     parser.add_argument('--regressor_params', type=str, default='./configs/linear_regression.json',
-                            help='File path to JSON string of regressor parameters.')
+                        help='File path to JSON string of regressor parameters.')
     parser.add_argument('--model_params', type=str, default='./configs/xgboost.json',
-                            help='File path to JSON string of model parameters.')
-
+                        help='File path to JSON string of model parameters.')
 
     args = parser.parse_args()
     args_dict = vars((args))
     main(args.target_column, args.model_name, args.model_params, args.regressor_name, args.regressor_params,
-             args.save_dir)
+         args.save_dir)
